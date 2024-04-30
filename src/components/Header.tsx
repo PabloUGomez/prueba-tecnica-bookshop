3d47b70e2973794f5f5ef8f266d7d46940d4621a
Pablo Gomez <pablogomez0430@gmail.com>
2024-04-30 14:38:33 -0300

    add favorites

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
+import { StarFull } from '../Icons/Star'
+import { Link } from 'react-router-dom'
 export default function Header() {
   return (
-    <header className="flex w-full justify-center">
-      <h1 className="text-6xl font-semibold">Don Quijote</h1>
+    <header className='flex w-full justify-center items-center'>
+      <h1 className='text-6xl font-semibold'>Don Quijote</h1>
+      <Link
+        to='/favorites'
+        className='flex justify-center items-center absolute right-12 gap-x-2 hover:text-yellow-400'
+      >
+        <h3>Favorites</h3>
+        <StarFull />
+      </Link>
     </header>
   )
 }
--- src/components/LibroCard.tsx
+++ src/components/LibroCard.tsx
@@ -1,10 +1,23 @@
+import { StarEmpty } from '../Icons/Star'
 import { type Book } from '../types'
 
 export default function LibroCard({ book }) {
-  const { title, pages, genre, cover, year } = book.book as Book
+  const { title, pages, genre, cover, year, ISBN } = book.book as Book
+
+  const handleClick = (ISBN : number) => {
+    
+  }
+
   return (
-    <article className='w-64 hover:bg-gray-900 p-4 rounded-lg'>
+    <article className='w-64 hover:bg-gray-900 p-4 rounded-lg group'>
+      <button
+        className='opacity-0 group-hover:opacity-100 absolute bg-gray-900 p-2 m-1'
+        onClick={() => handleClick(ISBN)}
+      >
+        <StarEmpty />
+      </button>
       <img src={cover} alt={title} />
+
       <samp>
         <h3>{title}</h3>
         <p>Año: {year}</p>
