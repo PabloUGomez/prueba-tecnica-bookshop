2bc041024bdfcdf1cce29d93f3c5c6e98bb75957
Pablo Gomez <pablogomez0430@gmail.com>
2024-05-03 12:55:40 -0300

    add localstorage

--- src/store/useBooks.tsx
+++ src/store/useBooks.tsx
@@ -1,4 +1,6 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
 import { Book } from '../types'
 
 interface BooksState {
@@ -11,47 +13,64 @@ interface BooksState {
   unSetFavoriteBook: (favoriteBook: Book) => void
   setGenre: (genre: string | undefined) => void
   setPages: (pages: number | undefined) => void
-  getBooksFiltered: (genge: string | undefined, pages: number | undefined) => Array<Book>
+  getBooksFiltered: (
+    genge: string | undefined,
+    pages: number | undefined
+  ) => Array<Book>
   getMaxPages: () => number
 }
 
-export const useBooks = create<BooksState>((set, get) => ({
-  books: [],
-  favoriteBooks: [],
-  genre: undefined,
-  pages: undefined,
-  setBooks: (books: Array<Book>) => set({ books }),
-  setFavoriteBook: (favoriteBook: Book) => {
-    set((state: { favoriteBooks: Book[] }) => {
-      const favoriteBooks = [...state.favoriteBooks, favoriteBook]
-      return { favoriteBooks }
-    })
-  },
-  unSetFavoriteBook: (favoriteBook: Book) => {
-    set((state: { favoriteBooks: Book[] }) => {
-      const favoriteBooks = state.favoriteBooks.filter(
-        (book) => book.ISBN !== favoriteBook.ISBN
-      )
-      return { favoriteBooks }
-    })
-  },
-  setGenre: (genre: string | undefined) => set({ genre }),
-  setPages: (pages: number | undefined) => set({ pages }),
-  getBooksFiltered: (genre: string | undefined, pages: number | undefined) => {
-    if (genre != undefined && pages != undefined)
-      return get()
-        .books.filter((book) => book.genre === genre)
-        .filter((book) => book.pages <= pages)
-    if (genre != undefined && pages === undefined)
-      return get().books.filter((book) => book.genre === genre)
-    if (genre === undefined && pages != undefined)
-        return get().books.filter((book) => book.pages <= pages)
-    return get().books
-  },
-  getMaxPages: () => {
-    // eslint-disable-next-line prefer-spread
-    const max =  Math.max.apply(Math, get().books.map((book) => book.pages))
-    if (max > 0 ) return max
-    return 1000
-  }
-}))
+export const useBooks = create<BooksState>()(
+  persist(
+    (set, get) => ({
+      books: [],
+      favoriteBooks: [],
+      genre: undefined,
+      pages: undefined,
+      setBooks: (books: Array<Book>) => set({ books }),
+      setFavoriteBook: (favoriteBook: Book) => {
+        set((state: { favoriteBooks: Book[] }) => {
+          const favoriteBooks = [...state.favoriteBooks, favoriteBook]
+          return { favoriteBooks }
+        })
+      },
+      unSetFavoriteBook: (favoriteBook: Book) => {
+        set((state: { favoriteBooks: Book[] }) => {
+          const favoriteBooks = state.favoriteBooks.filter(
+            (book) => book.ISBN !== favoriteBook.ISBN
+          )
+          return { favoriteBooks }
+        })
+      },
+      setGenre: (genre: string | undefined) => set({ genre }),
+      setPages: (pages: number | undefined) => set({ pages }),
+      getBooksFiltered: (
+        genre: string | undefined,
+        pages: number | undefined
+      ) => {
+        if (genre != undefined && pages != undefined)
+          return get()
+            .books.filter((book) => book.genre === genre)
+            .filter((book) => book.pages <= pages)
+        if (genre != undefined && pages === undefined)
+          return get().books.filter((book) => book.genre === genre)
+        if (genre === undefined && pages != undefined)
+          return get().books.filter((book) => book.pages <= pages)
+        return get().books
+      },
+      getMaxPages: () => {
+        // eslint-disable-next-line prefer-spread
+        const max = Math.max.apply(
+          Math,
+          get().books.map((book) => book.pages)
+        )
+        if (max > 0) return max
+        return 1000
+      },
+    }),
+    {
+      name: 'food-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+)
